ca933e2a28c54c9b8e69b227c4b87a5e0817fffc
deadpixelstudio <support@deadpixelstudio.co.uk>
2019-06-27 17:10:27 +0100

    Setup Autoload & PHPUnit

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace DeadPixelStudio\Lockdown\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class InitialTest extends TestCase
+{
+    /** @test */
+    function test()
+    {
+        $this->assertTrue(true);
+    }
+}
